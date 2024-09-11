ff79b1a5ab1c86ec95e93d7fb8e3ad638a80b61c
Rehanispro75 <rehanshaikh218053@gmail.com>
2024-09-11 21:38:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+// var variable1 = "Hello world";
+// document.write(variable1.toUpperCase())
+// document.write("<br>")
+// var variable2 = "hello world";
+// document.write(variable2.toLowerCase())
+// document.write("<br>")
+// document.write(variable2.endsWith("d"))
+// document.write("<br>")
+// document.write(variable2.startsWith("h"))
+
+
+var num =5
+
+for(i=1 ; i<=100000 ; i=i+1){
+
+document.write(num +"x" + i+"=" + num*i)
+document.write("<br>")
+}
+
+
+
+
+
